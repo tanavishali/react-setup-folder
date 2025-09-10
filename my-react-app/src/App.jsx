a69a1469e7da7364b2b76623397cf94a88fbe88f
Admin <your-email@example.com>
2025-09-10 12:53:33 +0500

    add new

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 
   return (
     <>
-    <h1 className='bg-amber-600'>i am setup</h1></>
+    <h1 className='bg-amber-600'>i am setup</h1>
+    <b className='text-amber-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ab recusandae ipsum nam voluptatibus quaerat dolorum esse beatae optio assumenda, minima modi, veritatis provident cum quasi reprehenderit libero reiciendis accusantium.</b>
+    </>
   )
 }
 
